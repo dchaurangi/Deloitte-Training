7df873d2ae0846cc9e1baaeb24ee6400eee26851
dchaurangi <54366076+dchaurangi@users.noreply.github.com>
2019-12-28 09:07:53 +0530

    Add files via upload

--- compare/Employee.java
+++ compare/Employee.java
@@ -1,13 +1,17 @@
 package compare;
 
-public class Employee {
+import java.util.Comparator;
+
+public class Employee implements Comparable<Employee>, Comparator<Employee>{
 	private String name;
 	private int id;
+	private double salary;
 	
-	public Employee(String name, int id) {
+	public Employee(String name, int id, double salary) {
 		super();
 		this.name = name;
 		this.id = id;
+		this.salary=salary;
 	}
 	
 	
@@ -33,6 +37,41 @@ public class Employee {
 	}
 	public void setId(int id) {
 		this.id = id;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+
+
+	@Override
+	public String toString() {
+		return "Employee [name=" + name + ", id=" + id + ", salary=" + salary + "]";
+	}
+
+
+	/*@Override
+	public int compareTo(Employee other)
+	{
+		 return this.id-other.id;
+	}*/
+	
+	@Override
+	public int compare(Employee e1, Employee e2)
+	{
+		return e1.name.compareToIgnoreCase(e2.name);
+	}
+	
+	@Override
+	public int compareTo(Employee other)
+	{
+		 return (int) (this.salary-other.salary);
 	}
 	
 	
